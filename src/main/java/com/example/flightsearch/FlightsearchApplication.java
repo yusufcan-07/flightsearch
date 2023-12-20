ab5078bf8ffb050d2e548553bc6472102244cdb1
Yusufcan Ulusoy <yusufcan.ulusoy@agu.edu.tr>
2023-12-20 23:51:46 +0300

    Initial commit

---
+++ src/main/java/com/example/flightsearch/FlightsearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.flightsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsearchApplication.class, args);
+	}
+
+}
